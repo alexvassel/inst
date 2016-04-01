a389b5d77daa1c330e0ef1c1ff89f7686a165d62
av <alexvassel@gmail.com>
2016-04-01 23:54:46 +0300

    updated models

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import json
 from furl import furl
 import requests
 
+from models import User, Stat, Media
+
 
 class Instagram(object):
     ACCESS_TOKEN = "487872815.1497896.ef9829ec8bf9414cb7d85967833670b5"
@@ -39,7 +41,6 @@ class Instagram(object):
 
     def get_user_info(self, user_id):
         url = self._update_url('/users/{}/'.format(user_id))
-        print url
         r = requests.get(url)
         response = self._parse_response(r.content)
         return response
@@ -48,12 +49,36 @@ class Instagram(object):
 if __name__ == '__main__':
     i = Instagram()
 
+    likes = dict()
+
     # Get user info
-    sample_user_data = i.get_user_info(i.SAMPLE_USER['id'])
-    print 'User data - {}'.format(sample_user_data)
+    # sample_user_data = i.get_user_info(i.SAMPLE_USER['id'])
+    # print 'User data - {}'.format(sample_user_data)
 
     # Get last media of the sample user
-    random_media_id = i.get_user_media(i.SAMPLE_USER['id'])[0]['id']
-    random_media_likes = i.get_media_likes(random_media_id)
+    medias = i.get_user_media(i.SAMPLE_USER['id'])
+
+    for media in medias:
+        likes[media['id']] = i.get_media_likes(media['id'])
+
+    for media_id, users in likes.iteritems():
+        media = Media(media_id=media_id)
+        media.save()
+        for user in users:
+            user_info = i.get_user_info(user['id'])
+            try:
+                user = User.select().where(User.user_id == user_info['id']).get()
+            except User.DoesNotExist:
+                user = User(username=user_info['username'], bio=user_info['bio'],
+                            website=user_info['website'], profile_picture=user_info['profile_picture'],
+                            user_id=user_info['id'], full_name=user_info['full_name'])
+                user_stat = Stat(followed_by=user_info['counts']['followed_by'],
+                                 follows=user_info['counts']['follows'],
+                                 media=user_info['counts']['media'])
+                user.stat = user_stat
+
+                user_stat.save()
+                user.save()
+            media.users.add([user])
     
-    print 'Likes of the media with id {} - {}'.format(random_media_id, random_media_likes)
+    # print 'Likes of the media with id {} - {}'.format(random_media_id, random_media_likes)
--- models.py
+++ models.py
@@ -7,6 +7,15 @@ from playhouse.fields import ManyToManyField
 db = SqliteDatabase('instagram.db')
 
 
+class Stat(Model):
+    followed_by = IntegerField()
+    follows = IntegerField()
+    media = IntegerField()
+
+    class Meta:
+        database = db
+
+
 class User(Model):
     username = CharField()
     bio = CharField()
@@ -14,22 +23,15 @@ class User(Model):
     profile_picture = CharField()
     full_name = CharField()
     user_id = CharField()
+    stat = ForeignKeyField(Stat)
 
     class Meta:
         database = db
 
 
 class Media(Model):
-    user = ManyToManyField(User, related_name='medias')
-
-    class Meta:
-        database = db
-
-
-class Stat(Model):
-    followed_by = IntegerField()
-    follows = IntegerField()
-    media = ForeignKeyField(rel_model=Media)
+    users = ManyToManyField(User, related_name='medias')
+    media_id = CharField()
 
     class Meta:
         database = db
@@ -38,6 +40,6 @@ class Stat(Model):
 def create_database():
     db.connect()
     try:
-        db.create_tables((User, Stat, Media))
+        db.create_tables((User, Stat, Media.users.get_through_model(), Media))
     except OperationalError:
         print 'database already exists'
